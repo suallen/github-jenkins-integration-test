931de594b2d28e8d91d8497c5ad0f82443c67711
suallen <suallen@sjinnovation.com>
2020-05-19 15:46:46 +0530

    Made changes to file to trigger the build

--- testOne.py
+++ testOne.py
@@ -2,3 +2,4 @@ for x in range(6):
     print("Welcome to Jenkins github Integration " + str(x))
 else:
     print("Yes. You successfully completed the first build.")
+    print("You completed more than 50%. Great job")
