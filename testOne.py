99aa46be99f514b719b18f1f890e34c6a135ff93
suallen <suallen@sjinnovation.com>
2020-06-18 11:14:28 +0530

    All 4 jobs are chained

--- testOne.py
+++ testOne.py
@@ -4,3 +4,4 @@ else:
     print("Yes. You successfully completed the first build.")
     print("You completed more than 50%. Great job")
     print("Testing for crons on deployment")
+    print("Next job is chained to this job")
